ea00802967e216ec3fe689be85c77c65d7faef44
Julian Norton <Julian@JulianNorton.com>
2016-01-14 23:42:32 -0500

    trying to copy some functions, editing so there's no inputs

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,9 +1,10 @@
 
-
-# thank you Al, http://inventwithpython.com/chapter10.html
+# SublimeREPL: Python run current file
+# a = raw_input("test raw input")
 
 import random
 
+# thank you Al, http://inventwithpython.com/chapter10.html
 def drawBoard(board):
 	# function prints out the board that it was passed
 	# 'board' is a list of 10 strings representing the board (ignore index 0)
@@ -19,28 +20,55 @@ def drawBoard(board):
 	print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 	print('   |   |')
 
-def inputPlayerLetter():
-	# Lets the player type which letter they want to be
-	# Returns a list with the player's letter as the first item, and the computer's letter as the second.
-	letter = ""
-	while not (letter == 'X') or (letter == 'O'):
-		print('Do you want to be X or O?')
-		letter = input().upper()
-
-	#The first element in the list is the player's letter, the second is the computer's latter
-	if letter == 'X':
-		# player's letter = X, computer's = O
-		return ['X', 'O']
-	else:
-		# player's letter = O, computer's = X
-		return['O','X']
-
 def whoGoesFirst():
 	# Randomly choose the player who goes first.
 	if random.randint(0,1) == 0:
 		return 'computer'
 	else:
 		return 'player'
-		
-# just trying to get python IDE working in sublime
-print("kdfgkldfgklfdkfdlklkgdf")
+
+def makeMove(board, letter, move):
+	board[move] = letter
+
+def isWinner(bo, le):
+	# Given a board and a player's letter, this function returns True if that player has won.
+	#we use bo instead of board and le instead of letter so we don't have to type
+	return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
+	(bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
+	(bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
+	(bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
+	(bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
+	(bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
+	(bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
+	(bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
+
+def getBoardCopy(board):
+	# Make a duplicate of the board list and return it as a duplicate.
+	dupeBoard = []
+
+	for i in board:
+		dupeBoard.append(i)
+
+	return dupeBoard
+
+def isSpaceFree(board, move):
+	#Return true if the passed move is free on the board.
+	return board[move] == ''
+
+move = ''	
+def getPlayerMove(board):
+	#player's move is random
+	move = ''
+	while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+		print('What is your next move? (1-9)')
+		# move = input()
+		# screw letting the player choose, randomize it!
+		move = random.randint(0,9)
+	return int(move)
+
+#print test code
+turn = whoGoesFirst()
+theBoard = [' '] * 10
+
+#drawBoard(theBoard)
+print turn + " goes first"
