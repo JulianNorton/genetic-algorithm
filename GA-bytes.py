51a6f031a5dee1ec67130efbf71efdb456d605ef
Julian Norton <Julian@JulianNorton.com>
2016-02-01 21:38:07 -0500

    cleaned out some unused code

--- GA-bytes.py
+++ GA-bytes.py
@@ -1,18 +1,14 @@
-# numpy used for random generation and percentile function
-#import numpy as np
 import random
 
 population = []
 generation_count = 0
-solution = '11111111'
-
 
 class Individual (object):
     class Chromosome(object):
         def __init__(self, length=0, length_max=8):
             # create a string that's made up of random bits
-            gene = ""
             # Append '0' or '1' until max
+            gene = ""
             for i in xrange(length_max):
                 gene += str(random.randint(0,1))
             else:
@@ -66,11 +62,7 @@ current_population.sort_by_fitness()
 current_population.dump_individuals(20)
 
 # current_population.kill_some_population()
-
-# print "########################################################"
 # current_population.dump_individuals(10)
-
-# print "########################################################"
 
 # for i in xrange(0, 11, 2):
 #     print(i)
@@ -79,14 +71,11 @@ current_population.dump_individuals(20)
 # half_gene_length = gene_length / 2
 # print "1/2 gene length = ", half_gene_length
 
-# print current_population.individuals[19].chromosome.gene
 # print "A1 | 1st half |", current_population.individuals[19].chromosome.gene[0:(half_gene_length)]
 # A1 = current_population.individuals[19].chromosome.gene[0:(half_gene_length)]
 # print "A2 | 2nd half |", current_population.individuals[19].chromosome.gene[(half_gene_length):]
 # A2 = current_population.individuals[19].chromosome.gene[0:(half_gene_length)]
 
-
-# print current_population.individuals[18].chromosome.gene
 # print "B1 | 1st half |", current_population.individuals[18].chromosome.gene[0:(half_gene_length)]
 # B1 = current_population.individuals[18].chromosome.gene[0:(half_gene_length)]
 # print "B2 | 2nd half |", current_population.individuals[18].chromosome.gene[(half_gene_length):]
@@ -106,40 +95,10 @@ current_population.dump_individuals(20)
 
 # print x
 
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # overloading operators
 # print population
 # could just do what dump is doing
-
-def check_solution():
-    global generation_count
-    global solution
-    if solution in population:
-        print generation_count
-    else:
-        generation_count = generation_count + 1
-        print "no solution"
-        print generation_count, "generation"
-
-# print ""
-# print "----------------------"
-# print "| testing code below |"
-# print "----------------------"
-# print ""
-
-
-# test = generate_chromosome()
-# print test[0:4], "|", test[4:8], "|| parent 1"
-# test1 = generate_chromosome()
-# print test1[0:4], "|", test1[4:8], "|| parent 2"
-# # take first 4 digits from parent 1, and last 4 from parent 2
-# test2 = test[0:4] + test1[4:8]
-# print test2[0:4], "|", test2[4:8], "|| child 1"
-# # take first 4 digits from parent 2, and last 4 from parent 1
-# test3 = test1[0:4] + test[4:8]
-# print test3[0:4], "|", test3[4:8], "|| child 2"
-
 
 # def gene_mutation():
 #     # 2% chance to randomly mutate a bit
@@ -154,4 +113,3 @@ def check_solution():
 #         print "No mutation"
 
 # gene_mutation()
-
