00193e090116c0516a628c2efb7448b0aa046bb2
Julian Norton <Julian@JulianNorton.com>
2016-02-14 14:04:07 -0500

    1.0 working! unoptimized

--- GA-bytes-1.0.py
+++ GA-bytes-1.0.py
@@ -1,14 +1,24 @@
 import random
-random.seed(1)
-chromosome_length_max = 8
-current_generations = 0
-max_generations = 40
-population_max = 8
+import sys
+
+################################################################
+
+# random.seed(1)
+
+chromosome_length_max = 64
+
+max_generations = 100000
+
+population_max = 1024
 population_current = list()
-# solution = '1' * chromosome_length_max
-# gene_replacement = random.randint(0,1)
-# mutation_chance = random.randint(0,24)
-# solution_found = False
+
+mutation_chance = random.randint(0,1) # 10%
+gene_replacement = random.randint(0,1)
+gene_replacement = '1'
+
+
+solution = '1' * chromosome_length_max
+################################################################
 
 def chromosome():
   gene = ""
@@ -32,30 +42,38 @@ def population_sorted():
 
 def population_status():
   print population_current
+  # print len(population_current)
   print 'pop count ==', len(population_current)
 
 def population_cull():
   i = 0
-  # population_status()
   while i < (population_max / 2):
-    # print population_current[-1]
     del population_current[-1]
     i = i + 1
 
+
+def chromosome_mutation(chromosome):
+    chromosome = list(chromosome)
+    gene_location = random.randint(0,(chromosome_length_max - 1))
+    if mutation_chance == 0:
+      chromosome[gene_location] = str(gene_replacement)
+      chromosome = ''.join(chromosome)
+      return chromosome
+    else:
+      chromosome = ''.join(chromosome)
+      return chromosome
+
 def population_reproduction():
-  # population_status()
   population_survivors = list()
   for k, v in population_current:
     population_survivors.append(v)
   for parent_A, parent_B in zip(*[iter(population_survivors)]*2):
-    # print parent_A, 'parent A'
-    # print parent_B, 'parent B'
     child_A = parent_A[:(chromosome_length_max/2)] + parent_B[(chromosome_length_max/2):chromosome_length_max]
-    # child_A = gene_mutation(child_A)
+    child_A = chromosome_mutation(child_A)
     chromosome_scored = chromosome_fitness(child_A), child_A
     population_current.append(chromosome_scored)
     child_B = parent_B[:(chromosome_length_max/2)] + parent_A[(chromosome_length_max/2):chromosome_length_max]
-    # child_B = gene_mutation(child_B)
+    child_B = chromosome_mutation(child_B)
     chromosome_scored = chromosome_fitness(child_B), child_B
     population_current.append(chromosome_scored)
 
@@ -64,24 +82,38 @@ def population_fitness_average():
   for k, v in population_current:
     fitness_list.append(k)
   # print 'Fitness list:', fitness_list
-  print 'AVERAGE Fit:', sum(fitness_list) / len(fitness_list)
+  print 'Average Fit ==', sum(fitness_list) / len(fitness_list)
   # print 'test'
 
   # print 'survivors', population_survivors
 
-population_generate()
-def test():
-  population_fitness_average()
-  population_sorted()
-  population_cull()
-  population_reproduction()
+def solution_checker():
+  for k, v in population_current:
+    # print v
+    if v == solution:
+      population_status()
+      population_fitness_average()
+      print '******************'
+      print '--------------------'
+      print 'SOLUTION FOUND!'
+      print '--------------------'
+      print '******************'
+      sys.exit(0)
 
-population_status()
-test()
-test()
-test()
-test()
-test()
-population_status()
-print len(population_current)
+def generation_iterate():
+    population_cull()
+    population_reproduction()
+    population_sorted()
+    solution_checker()
+
+def epoch_generate():
+  population_generate()
+  population_sorted()
+  for i in xrange(max_generations):
+    generation_iterate()
+    population_fitness_average()
+    print 'Generation count ==', i
+  population_status()
+
+epoch_generate()
 
