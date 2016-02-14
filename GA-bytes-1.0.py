39e1034f5f21e9f606405399cecf569cfc046870
Julian Norton <Julian@JulianNorton.com>
2016-02-14 12:26:06 -0500

    changed file names, got 1.0 working

--- GA-bytes-1.0.py
+++ GA-bytes-1.0.py
@@ -2,8 +2,8 @@ import random
 random.seed(1)
 chromosome_length_max = 8
 current_generations = 0
-max_generations = 4
-population_max = 4
+max_generations = 40
+population_max = 8
 population_current = list()
 # solution = '1' * chromosome_length_max
 # gene_replacement = random.randint(0,1)
@@ -50,11 +50,11 @@ def population_reproduction():
   for parent_A, parent_B in zip(*[iter(population_survivors)]*2):
     # print parent_A, 'parent A'
     # print parent_B, 'parent B'
-    child_A = parent_A[0:4] + parent_B[4:8]
+    child_A = parent_A[:(chromosome_length_max/2)] + parent_B[(chromosome_length_max/2):chromosome_length_max]
     # child_A = gene_mutation(child_A)
     chromosome_scored = chromosome_fitness(child_A), child_A
     population_current.append(chromosome_scored)
-    child_B = parent_B[0:4] + parent_A[4:8]
+    child_B = parent_B[:(chromosome_length_max/2)] + parent_A[(chromosome_length_max/2):chromosome_length_max]
     # child_B = gene_mutation(child_B)
     chromosome_scored = chromosome_fitness(child_B), child_B
     population_current.append(chromosome_scored)
@@ -63,17 +63,25 @@ def population_fitness_average():
   fitness_list = list()
   for k, v in population_current:
     fitness_list.append(k)
-  print fitness_list
-  print sum(fitness_list) / len(fitness_list)
-  print 'test'
+  # print 'Fitness list:', fitness_list
+  print 'AVERAGE Fit:', sum(fitness_list) / len(fitness_list)
+  # print 'test'
 
   # print 'survivors', population_survivors
 
 population_generate()
-population_fitness_average()
-population_sorted()
-population_cull()
-# population_reproduction()
-# population_sorted()
+def test():
+  population_fitness_average()
+  population_sorted()
+  population_cull()
+  population_reproduction()
 
+population_status()
+test()
+test()
+test()
+test()
+test()
+population_status()
+print len(population_current)
 
